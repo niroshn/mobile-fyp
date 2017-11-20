8bc62ca21007691857df1e1689bd45f6d8f51d55
niroshn <niroshanrd.13@cse,mrt.ac.lk>
2017-11-20 22:09:46 +0530

    new

--- src/pages/myproject/myproject.ts
+++ src/pages/myproject/myproject.ts
@@ -127,7 +127,7 @@ export class Myproject {
       title:'Medication',
       text: 'You have to get Medication at ',
       at:time,
-      sound:'res://assets/alrt.mp3',
+      sound:'file://assets/alrt.mp3',
       data: {mydata:'Information'}
     });
   }
