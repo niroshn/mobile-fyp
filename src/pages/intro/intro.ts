df29d0169671a147ef082fe330337a8875a3a12a
niroshn <niroshanrd.13@cse,mrt.ac.lk>
2017-11-20 11:46:37 +0530

    emergency added

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,7 @@
 import { Component,ViewChild } from '@angular/core';
 import { IonicPage,NavController,Slides } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -10,10 +12,26 @@ export class Intro {
 
 @ViewChild('mySlider') slider: Slides;
 currentIndex:any;
+currentUser :any;
+msg:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private storage: Storage,public http: Http) {
 
-this.currentIndex = 0;
+
+ this.currentIndex = 0;
+ 
+ this.storage.get('user').then((val) => {
+  console.log(val);
+  if(val){
+    this.currentUser=val;
+    this.msg = this.currentUser.title +" "+this.currentUser.firstname+ "  is in Emergency Situation. Lets' Contact now";
+    console.log(this.msg);
+  }
+  else{
+    
+  }
+  
+});
   }
 
   onSlideChanged() {
@@ -25,7 +43,33 @@ this.currentIndex = 0;
     }
 
     signin() {
-this.navCtrl.push('Home');
+       this.navCtrl.push('Home');
+}
+
+emergency(){
+  let relations= this.currentUser.relations;
+  console.log(relations);
+
+
+  
+  
+
+ for (let l of relations) {
+  let massage = {  
+    "tel":l.tel,
+    "sms":this.msg
+ 
+ }
+  this.http.post("https://r0wl6iaxea.execute-api.us-east-1.amazonaws.com/dev/notifications/notifications", massage)
+  .subscribe(data => {
+    
+
+  }, error => {
+    console.log(error);// Error getting the data
+  });
+}
+ 
+  
 }
 
 
