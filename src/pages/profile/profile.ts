7f8626e969bec5f131d368bb66033086b60f7a2e
nuwanrathnayaka <nuwanrathnayake.icc@gmail.com>
2017-11-22 16:56:41 +0530

    edits nuwan

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController,NavParams, ModalController } from 'ionic-angular';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Editprofile } from '../editprofile/editprofile';
+import { Storage } from '@ionic/storage';
+
 
 @IonicPage()
 @Component({
@@ -14,31 +16,33 @@ export class Profile {
   user_params: any;
   newRelation: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public http: Http) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController,private storage: Storage,public navParams: NavParams, public modalCtrl: ModalController, public http: Http) {
     this.user_params = {
       "user_id": "nuwan@gmail.com"
     }
+        this.user = { "_id": "59a3ba9d734d1d7ab9ead016", "user_id": "nuwan@gmail.com", "nic": "920123223V", "title": "Mr.", "firstname": "Vijhkuhndula", "lastname": "Fernalkijihndo", "birthdate": "1992-01-24", "gender": "Male", "maritalstatus": "Single", "bloodtype": "A+", "alcoholic": true, "phones": { "home": "+94112826896", "mobile": "+94776711781" }, "email": "dinukav.fernando@gmail.com", "address": { "street": "Anderson Road", "city": "Colombo 01", "country": "Sri Lanka" }, "avatar": "no-avatar.jpg", "relations": [{ "first_name": "nushan", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Brother" }, { "first_name": "shan", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Brother" }, { "first_name": "nun", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Father" }, { "first_name": "nushani", "last_name": "Bond", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080909", "relation": "Sister" }, { "first_name": "nushani", "last_name": "Bond", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080909", "relation": "Sister" }] };
 
-    this.user = { "_id": "59a3ba9d734d1d7ab9ead016", "user_id": "nuwan@gmail.com", "nic": "920123223V", "title": "Mr.", "firstname": "Vindula", "lastname": "Fernando", "birthdate": "1992-01-24", "gender": "Male", "maritalstatus": "Single", "bloodtype": "A+", "alcoholic": true, "phones": { "home": "+94112826896", "mobile": "+94776711781" }, "email": "dinukav.fernando@gmail.com", "address": { "street": "Anderson Road", "city": "Colombo 01", "country": "Sri Lanka" }, "avatar": "no-avatar.jpg", "relations": [{ "first_name": "nushan", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Brother" }, { "first_name": "shan", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Brother" }, { "first_name": "nun", "last_name": "Bandra", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080907", "relation": "Father" }, { "first_name": "nushani", "last_name": "Bond", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080909", "relation": "Sister" }, { "first_name": "nushani", "last_name": "Bond", "address": "No 89/9 Mihin Road Colombo 3", "tel": "+94772080909", "relation": "Sister" }] };
-
-    this.http.post("https://r0wl6iaxea.execute-api.us-east-1.amazonaws.com/dev/users/getCurrentUser", this.user_params)
-      .subscribe(data => {
-
-        this.user = data.json().data[0];
-        console.log(JSON.stringify(this.user));
-        try {
-          this.newRelation = navParams.get("user");
-          console.log(this.newRelation);
-          this.addNewRelation(this.newRelation);
-        } catch (e) {
-          console.log("didnt get the new relation");
-        }
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loading.present();
+    this.storage.get('user').then((val) => {
+      console.log('Your age is', val);
+      if(val){
+        console.log(val);
+        this.user = val;
+        //here
+        loading.dismiss(); 
+      }
+      else{
         
-      }, error => {
-        console.log(error);// Error getting the data
-      });
-
+      }
       
+    });
+
+    
+
+     
   }
 
   goBack(){
